cec69748db9db3ad7aa0c2108479bc4e1b22b641
roule <roule@LAPTOP-CHN7553L>
2021-11-26 19:10:13 -0500

    Test pour premier commit

---
+++ helloworld/src/test/java/com/openclassrooms/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,15 @@
+package com.openclassrooms.helloworld;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class HelloworldApplicationTests {
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
